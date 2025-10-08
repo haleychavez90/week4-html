b006cd7e22a4b973b861cc72658fc7043164c6f4
Haley Chavez <haley.chavez@yahoo.com>
2025-10-07 20:36:54 -0500

    day 5 added state.js and finish pets.js to complete the full log and code. The form is now in the works as it should be

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function saveForm() {
 
 // Event Listener Button
 document.getElementById("submitBtn").addEventListener("click", saveForm);
+
+// Local Storage
+localStorage.setItem("name", "Haley");
+localStorage.setItem("age", "29");
+localStorage.setItem("species", "Dog");
+localStorage.setItem("energy", "80");
+localStorage.setItem("happiness", "90");
+
+// Proof of Life
+console.log("Hello from script.js");
