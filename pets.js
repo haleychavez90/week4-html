cd879cc2b73ab17c3c778376ca2c2d1d24ad6e86
Haley Chavez <haley.chavez@yahoo.com>
2025-10-07 20:28:28 -0500

    project has day4, completing pets.js pets.html and petCreator.js to make active code

--- pets.js
+++ pets.js
@@ -19,6 +19,7 @@ myPet.sleep();
 console.log(myPet.status());
 
 // Event Listener for Buttons
+document.getElementById("petsContainer").innerHTML = pets.map(pet => pet.petToHTML()).join("");
 document.getElementById("playButton").addEventListener("click", function() {
     myPet.play();
     document.getElementById("status").innerText = myPet.status();
@@ -32,11 +33,25 @@ document.getElementById("playButton").addEventListener("click", function() {
 
 // Create and Manage Pets
 const pets = [];
-pets.push(createPet("Buddy", 2, "Dog", 60, 80));
-pets.push(createPet("Mittens", 1, "Cat", 70, 90));
-pets.push(createPet("Goldie", 1, "Fish", 80, 70));
-pets.push(createPet("Tweety", 3, "Bird", 50, 60));
+pets.push(createPet("Lee", 2, "Dog", 60, 80));
+pets.push(createPet("Pinky", 1, "Cat", 70, 90));
+pets.push(createPet("Bubbles", 1, "Fish", 80, 70));
+pets.push(createPet("Flip", 3, "Bird", 50, 60));
 
 // Proof of Life
 console.log("Hello from pets.js");
 console.log("Hello from petCreator.js");
+
+// Create a Pet
+const pet = createPet("Wren", 4, "Dog", 70, 80);
+document.getElementById("petInfo").innerHTML = `
+    <h2>Pet Information</h2>
+    <p>${pet.status()}</p>
+`;  
+
+//Render Pets to HTML
+function renderPets(petArray) {
+    const petContainer = document.getElementById("petContainer");
+    petContainer.innerHTML = petArray.map(pet => pet.petToHTML()).join("");
+}
+renderPets(pets);
